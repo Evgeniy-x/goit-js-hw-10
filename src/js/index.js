0fe7de42b5276dff8a03031abec9ca12b7ffc947
Evgeniy <yaremenko.x@gmail.com>
2023-04-17 15:02:07 +0300

    done hw-10

---
+++ src/js/index.js
@@ -0,0 +1,48 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
+import '../css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+import { countryMarkup, countriesMarkup } from './markup';
+
+const DEBOUNCE_DELAY = 300;
+
+refs = {
+  input: document.querySelector('#search-box'),
+  listCountry: document.querySelector('.country-list'),
+  infoCountry: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  const searchData = e.target.value.trim();
+
+  if (!searchData) {
+    refs.listCountry.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(searchData)
+    .then(findCountries)
+    .catch(onFetchError)
+}
+
+function findCountries(coutriesArr) {
+  if (coutriesArr.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    refs.listCountry.innerHTML = '';
+    return;
+  }
+    
+  if(coutriesArr.length === 1) {
+      refs.listCountry.innerHTML = countryMarkup(coutriesArr[0]);
+    } else {
+      refs.listCountry.innerHTML = countriesMarkup(coutriesArr);
+    }
+}
+
+
+function onFetchError() {
+    Notify.failure('Oops, there is no country with that name');
+}
---
+++ src/js/markup.js
@@ -0,0 +1,35 @@
+export { countryMarkup, countriesMarkup };
+
+function countryMarkup({
+  name,
+  capital,
+  population,
+  flags,
+  languages: languagesCodes,
+}) {
+  const listLanguages = Object.values(languagesCodes).join(', ');
+
+  return `<li class="target-country">
+      <div class="country-wrapper">
+        <img class="icon" src='${flags.png}'>
+        <p class="country-name">${name.common}</p>
+      </div>
+      <ul class="info-list">
+        <li class="info-item">Capital: <span>${capital.join('')}</span></li>
+        <li class="info-item">Population: <span>${population}</span></li>
+        <li class="info-item">Languages: <span>${listLanguages}</span></li>
+      </ul>
+    </li>`;
+}
+
+function countriesMarkup(countriesList) {
+  return countriesList
+    .map(
+      ({ name, flags }) =>
+        `<li class="country-wrapper country-target">
+          <img class="icon" src='${flags.png}'>
+          <p class="country-name">${name.common}</p>
+        </li>`
+    )
+    .join('');
+}
